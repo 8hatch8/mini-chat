238d12e87eb55f8db7259dbb11de3dd9f1c39a63
8hatch8 <yutaka.pg@gmail.com>
2021-12-23 19:18:11 +0900

    Update job

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,7 +1,16 @@
 class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  # ブロードキャスト処理
+  def perform(message)
+    # room_channel.jsのreceivedメソッドへ { message: render_message[message]（中身はビューファイル） }を送信
+    ActionCable.server.broadcast "room_channel", message: render_message(message)
+  end
+
+  private
+
+  def render_message(message)
+    ApplicationController.renderer.render(partial: "messages/message", locals: { message: message })
+    # コントローラーのアクションの制約をうけずにビューファイルをレンダリング
   end
 end
